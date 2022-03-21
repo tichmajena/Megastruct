37b288a83e90d2d6da67f7625f759515f0cbfcb4
Tichakunda Majena <tichmajena@gmail.com>
2022-03-21 07:20:42 +0200

    routine

--- src/lib/js/store.js
+++ src/lib/js/store.js
@@ -2,4 +2,4 @@ import { writable } from "svelte/store";
 
 export let hide_menu = writable(false);
 export let hide_logo = writable(false);
-export let scroll_y = writable();
+export let scroll_y = writable(0);
