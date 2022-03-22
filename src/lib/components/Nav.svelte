1f95163aac384cfef2e4db8201972deb9efb26b0
tmajena <tichmajena@gmail.com>
2022-03-22 09:36:48 +0200

    fixed hero menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -26,16 +26,16 @@
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
   <div
     class="drawer-content flex flex-col "
-    on:scroll={(e) => {
+    on:scroll="{(e) => {
       $scroll_y = e.srcElement.scrollTop;
-      console.log("scrolling", e.srcElement.scrollTop);
-    }}
+      console.log('scrolling', e.srcElement.scrollTop);
+    }}"
   >
     <!-- Navbar -->
 
     <div
-      class:hidden={$hide_menu && $page.url.pathname === "/"}
-      class="w-full navbar bg-red-600 z-50 "
+      class:hidden="{$hide_menu && $page.url.pathname === '/'}"
+      class="w-full navbar bg-red-600 z-50 fixed"
     >
       <div class="flex-none lg:hidden">
         <label for="my-drawer-3" class="btn btn-square btn-ghost">
@@ -48,17 +48,15 @@
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            /></svg
+              d="M4 6h16M4 12h16M4 18h16"></path></svg
           >
         </label>
       </div>
       <div class="flex-1 px-2 mx-2">
         <div
-          class:hidden={!fixed}
           class="absolute top-0 left-20 p-2 bg-white rounded-b-sm shadow-md w-16"
         >
-          <img src={logo_icon} alt="" />
+          <img src="{logo_icon}" alt="" />
         </div>
       </div>
       <div class="flex-none hidden lg:block">
@@ -69,48 +67,48 @@
             <div
               transition:scale
               class="p-0.5 absolute w-1/2 mx-auto left-1/2 -translate-x-1/2 -bottom-2 bg-white z-60"
-              class:hidden={$page.url.pathname !== "/"}
-            />
+              class:hidden="{$page.url.pathname !== '/'}"
+            ></div>
           </li>
           <li class="relative">
             <a href="/about">About</a>
             <div
               transition:scale
               class="p-0.5 absolute w-1/2 mx-auto left-1/2 -translate-x-1/2 -bottom-2 bg-white z-60"
-              class:hidden={$page.url.pathname !== "/about"}
-            />
+              class:hidden="{$page.url.pathname !== '/about'}"
+            ></div>
           </li>
           <li class="relative">
             <a href="/services">Services</a>
             <div
               transition:scale
               class="p-0.5 absolute w-1/2 mx-auto left-1/2 -translate-x-1/2 -bottom-2 bg-white z-60"
-              class:hidden={$page.url.pathname !== "/services"}
-            />
+              class:hidden="{$page.url.pathname !== '/services'}"
+            ></div>
           </li>
           <li class="relative">
             <a href="/team">Team</a>
             <div
               transition:scale
               class="p-0.5 absolute w-1/2 mx-auto left-1/2 -translate-x-1/2 -bottom-2 bg-white z-60"
-              class:hidden={$page.url.pathname !== "/team"}
-            />
+              class:hidden="{$page.url.pathname !== '/team'}"
+            ></div>
           </li>
           <li class="relative">
             <a href="/projects">Projects</a>
             <div
               transition:scale
               class="p-0.5 absolute w-1/2 mx-auto left-1/2 -translate-x-1/2 -bottom-2 bg-white z-60"
-              class:hidden={$page.url.pathname !== "/projects"}
-            />
+              class:hidden="{$page.url.pathname !== '/projects'}"
+            ></div>
           </li>
           <li class="relative">
             <a href="/contact">Contact</a>
             <div
               transition:scale
               class="p-0.5 absolute w-1/2 mx-auto left-1/2 -translate-x-1/2 -bottom-2 bg-white z-60"
-              class:hidden={$page.url.pathname !== "/contact"}
-            />
+              class:hidden="{$page.url.pathname !== '/contact'}"
+            ></div>
           </li>
         </ul>
       </div>
@@ -120,13 +118,13 @@
     <slot><!-- optional fallback --></slot>
   </div>
   <div class="drawer-side ">
-    <label for="my-drawer-3" class="drawer-overlay" />
+    <label for="my-drawer-3" class="drawer-overlay"></label>
     <ul class="menu p-4 overflow-y-auto w-80 bg-red-700">
       <!-- Sidebar content here -->
       <li><a href="/">Home</a></li>
       <li class="relative">
         <a href="/about">About</a>
-        <div class="p-0.5 absolute w-full -bottom-2 bg-white z-60" />
+        <div class="p-0.5 absolute w-full -bottom-2 bg-white z-60"></div>
       </li>
     </ul>
   </div>
